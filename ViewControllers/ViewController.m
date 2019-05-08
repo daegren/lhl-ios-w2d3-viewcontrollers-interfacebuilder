73c86f738c7d51fd8f44f97ed19535599331c2d5
David Mills <dave@1morepx.com>
2019-05-08 11:47:29 -0400

    Code from lecture

--- ViewControllers/ViewController.m
+++ ViewControllers/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "SecondViewController.h"
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *mainLabel;
+@property (weak, nonatomic) IBOutlet UIStepper *stepper;
+@property (weak, nonatomic) IBOutlet UILabel *stepperLabel;
 
 @end
 
@@ -17,7 +22,24 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
+
+  self.mainLabel.text = @"Hello, Class!";
+  self.stepperLabel.text = [NSString stringWithFormat:@"%0.f", self.stepper.value];
 }
 
+- (IBAction)stepperValueChanged:(id)sender {
+ self.stepperLabel.text = [NSString stringWithFormat:@"%0.f", self.stepper.value];
+}
+
+- (IBAction)buttonTapped:(id)sender {
+  [self performSegueWithIdentifier:@"next" sender:self];
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+  if ([segue.identifier isEqualToString:@"next"]) {
+    SecondViewController *dvc = segue.destinationViewController;
+    dvc.name = @"Dave";
+  }
+}
 
 @end
